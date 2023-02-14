e9f29684cec6def9609a93dafffe0b2ebac5b13c
sanghee01 <sang6299@naver.com>
2023-02-14 15:37:19 +0900

    styled-components 도입

--- src/App.js
+++ src/App.js
@@ -1,47 +1,71 @@
-import "./Style.css";
+import styled from "styled-components";
 import InputGroup from "./InputGroup";
+import GlobalStyle from "./GlobalStyles";
+
+const Form = styled.form`
+  display: flex;
+  flex-direction: column;
+`;
+
+const Header = styled.h1`
+  margin: 10px auto;
+`;
+
+const Button = styled.button`
+  margin-top: 40px;
+  padding: 14px;
+  border-radius: 30px;
+  border: none;
+  background-color: #1889e6;
+  color: white;
+  font-size: 15px;
+  font-weight: bold;
+`;
 
 const App = () => {
   return (
-    <form action="#">
-      <h1>회원가입</h1>
-      <InputGroup
-        name="이메일"
-        type="email"
-        text="이메일을 입력하세요"
-        label="email"
-        isRequired="true"
-      />
-      <InputGroup
-        name="비밀번호"
-        type="password"
-        text="비밀번호을 입력하세요"
-        label="password"
-        isRequired="true"
-      />
-      <InputGroup
-        name="비밀번호 재확인"
-        type="password"
-        text="비밀번호을 입력하세요"
-        label="re-password"
-        isRequired="true"
-      />
-      <InputGroup
-        name="이름"
-        type="text"
-        text="이름을 입력하세요"
-        label="name"
-        isRequired="true"
-      />
-      <InputGroup
-        name="나이"
-        type="number"
-        text="나이를 입력하세요"
-        label="age"
-        isRequired={null}
-      />
-      <button>가입하기</button>
-    </form>
+    <>
+      <GlobalStyle />
+      <Form action="#">
+        <Header>회원가입</Header>
+        <InputGroup
+          name="이메일"
+          type="email"
+          text="이메일을 입력하세요"
+          label="email"
+          isRequired="true"
+        />
+        <InputGroup
+          name="비밀번호"
+          type="password"
+          text="비밀번호을 입력하세요"
+          label="password"
+          isRequired="true"
+        />
+        <InputGroup
+          name="비밀번호 재확인"
+          type="password"
+          text="비밀번호을 입력하세요"
+          label="re-password"
+          isRequired="true"
+        />
+        <InputGroup
+          name="이름"
+          type="text"
+          text="이름을 입력하세요"
+          label="name"
+          isRequired="true"
+        />
+        <InputGroup
+          name="나이"
+          type="number"
+          text="나이를 입력하세요"
+          label="age"
+          isRequired={null}
+        />
+        <Button>가입하기</Button>
+      </Form>
+    </>
   );
 };
 
--- src/InputGroup.js
+++ src/InputGroup.js
@@ -1,19 +1,44 @@
-import "./Style.css";
+import styled from "styled-components";
+
+const Title = styled.div`
+  margin-top: 15px;
+`;
+
+const Essential = styled.span`
+  margin-left: 5px;
+  font-size: 13px;
+`;
+
+const Aster = styled.span`
+  color: red;
+`;
+
+const Input = styled.input`
+  padding: 14px 25px;
+  border-radius: 5px;
+  border: 1px solid lightgray;
+  ::-webkit-outer-spin-button,
+  ::-webkit-inner-spin-button {
+    -webkit-appearance: none;
+    margin: 0;
+  }
+`;
+
 const InputGroup = (props) => {
   const { label, name, type, text, isRequired } = props;
   return (
     <>
-      <div className="title">
+      <Title>
         <label for={label}>{name}</label>
-        <span className="essential">
+        <Essential>
           {isRequired ? (
             <sup>
-              필수 <span className="aster">*</span>
+              필수 <Aster>*</Aster>
             </sup>
           ) : null}
-        </span>
-      </div>
-      <input
+        </Essential>
+      </Title>
+      <Input
         type={type}
         id={label}
         placeholder={text}
